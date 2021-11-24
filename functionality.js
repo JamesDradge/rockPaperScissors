73ed7567145025c055a7acf6f9340ed5f91aff1e
JamesDradge <bobdradge@hotmail.com>
2021-11-24 21:01:02 +0000

    setup JS file and wrote pseudo code

---
+++ functionality.js
@@ -0,0 +1,32 @@
+// computerPlay()
+// generates a random number between 1-3
+// assign number 1-3 to rock, paper, or scissors
+// return choice as computer selection
+
+// playerPlay()
+// player prompt
+// validate typed option is rock, paper, or scissors,
+// regulate Casing
+// return player selection
+
+// determineWinner()
+// rock > scissors
+// paper > rock
+// scissors > paper
+// check each selection
+// return the Winner
+
+// console.log the winner
+// declare scores
+// winner score increase
+
+// Game()
+// UNTIL score == 5
+// computerPlay
+// playerPlay
+// determineWinner
+// declare winner variable (global)
+// set winner variable
+
+
+// console log winner
